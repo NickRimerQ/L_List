d5fc91b76986ce795d292532c917582d2e0b9cee
NickRimerQ <58778313+NickRimerQ@users.noreply.github.com>
2021-03-21 21:42:15 +0300

    List

---
+++ List(1)/linked_list.cpp
@@ -0,0 +1,136 @@
+#include "linked_list.h"
+
+template<typename T>
+List<T>::List()
+{
+	Size = 0;
+	head = nullptr;
+}
+
+template<typename T>
+List<T>::~List()
+{
+	clear();
+}
+
+template<typename T>
+void List<T>::push_back(T data)
+{
+	if (head == nullptr)
+	{
+		head = new Node<T>(data);
+	}
+	else {
+		Node<T>* current = this->head;
+
+		while (current->pNext != nullptr)
+		{
+			current = current->pNext;
+		}
+		current->pNext = new Node<T>(data);
+
+	}
+	Size++;
+}
+
+template<typename T>
+int List<T>::GetSize()
+{
+	return Size;
+}
+
+template<typename T>
+T& List<T>::operator[](const int index)
+{
+	int k = 0; //счетчик
+	Node<T>* temp = this->head;
+	while (temp != nullptr)
+	{
+		if (k == index)
+		{
+			return temp->data;
+		}
+		temp = temp->pNext;
+		k++;
+	}
+}
+
+template<typename T>
+void List<T>::pop_front()
+{
+	Node<T>* temp = head;
+	head = head->pNext;
+	delete temp;
+	Size--;
+}
+
+template<typename T>
+void List<T>::clear()
+{
+	while (Size)
+	{
+		pop_front();
+	}
+}
+
+template<typename T>
+void List<T>::push_front(T data)
+{
+	head = new Node<T>(data, head);
+	Size++;
+}
+
+template<typename T>
+void List<T>::insert(T data, int index)
+{
+	if (index == 0)
+	{
+		push_front(data);
+	}
+	else
+	{
+		Node<T>* previous = this->head;
+
+		for (int i = 0; i < index - 1; i++)
+		{
+			previous = previous->pNext;
+		}
+
+		Node<T>* newNode = new Node<T>(data, previous->pNext);
+
+		previous->pNext = newNode;
+
+		Size++;
+	}
+}
+
+template<typename T>
+void List<T>::removeAt(int index)
+{
+	if (index == 0)
+	{
+		pop_front();
+	}
+	else
+	{
+		Node<T>* previous = this->head;
+		for (int i = 0; i < index - 1; i++)
+		{
+			previous = previous->pNext;
+		}
+
+		Node<T>* toDelete = previous->pNext;
+
+		previous->pNext = toDelete->pNext;
+
+		delete toDelete;
+
+		Size--;
+	}
+}
+
+template<typename T>
+void List<T>::pop_back()
+{
+	removeAt(Size - 1);
+}
---
+++ List(1)/linked_list.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+
+template<typename T>
+class List
+{
+	template<typename T>
+	class Node
+	{
+	public:
+		Node* pNext;
+		T data;
+		Node(T data_ = T(), Node* pNext_ = nullptr)
+		{
+			data = data_;
+			pNext = pNext_; 
+		}
+	};
+
+	int Size;
+	Node<T>* head;
+
+public:
+	List(); //конструктор
+	~List(); //деструктор
+	void push_back(T data); //добавл. эл-та в конец
+	int GetSize(); //возвращает размер списка
+	T& operator[](const int index);
+	void pop_front(); //удаление первого эл-та в списке
+	void clear(); //очищение списка
+	void push_front(T data); //добавление эл-та в начало списка
+	void insert(T data, int index); //добавление любого эл-та по индексу
+	void removeAt(int index); //удаление любого эл-та по индексу
+	void pop_back(); //удаление последнего эл-та
+};
